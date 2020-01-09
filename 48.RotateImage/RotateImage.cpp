9116ea8396e0452ce51edcdeabc23c880f410c0b
0Kelvins <554961401@qq.com>
2020-01-09 16:10:04 +0800

    48. Rotate Image

---
+++ 48.RotateImage/RotateImage.cpp
@@ -0,0 +1,45 @@
+/**
+ * 48. Rotate Image
+ * easy
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size() - 1;
+        int t;
+        for (int i = 0; i <= n / 2; i++)
+        {
+            for (int j = i; j < n - i; j++)
+            {
+                t = matrix[i][j];
+                matrix[i][j] = matrix[n - j][i];
+                matrix[n - j][i] = matrix[n - i][n - j];
+                matrix[n - i][n - j] = matrix[j][n - i];
+                matrix[j][n - i] = t;
+            }
+        }
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<vector<int>> m1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    vector<vector<int>> m2 = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+    vector<vector<int>> m3 = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}};
+    s.rotate(m3);
+    for (auto &&i : m3)
+    {
+        for (auto &&j : i)
+            cout << j << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
