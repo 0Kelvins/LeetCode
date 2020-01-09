7d1e076d69892f9e2b9862fc6de7f18527a3a142
0Kelvins <554961401@qq.com>
2020-01-09 16:20:26 +0800

    medium solution

--- 48.RotateImage/RotateImage.cpp
+++ 48.RotateImage/RotateImage.cpp
@@ -1,9 +1,9 @@
 /**
  * 48. Rotate Image
- * easy
  */
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution
@@ -23,6 +23,24 @@ public:
                 matrix[n - i][n - j] = matrix[j][n - i];
                 matrix[j][n - i] = t;
             }
+        }
+    }
+
+    /*
+    * clockwise rotate
+    * Medium
+    * first reverse up to down, then swap the symmetry 
+    * 1 2 3     7 8 9     7 4 1
+    * 4 5 6  => 4 5 6  => 8 5 2
+    * 7 8 9     1 2 3     9 6 3
+    */
+    void rotate(vector<vector<int>> &matrix)
+    {
+        reverse(matrix.begin(), matrix.end());
+        for (int i = 0; i < matrix.size(); ++i)
+        {
+            for (int j = i + 1; j < matrix[i].size(); ++j)
+                swap(matrix[i][j], matrix[j][i]);
         }
     }
 };
