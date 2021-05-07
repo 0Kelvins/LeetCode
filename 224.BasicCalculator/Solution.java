80e3e157af5bdeacd601d4f38ddaf77bfee8bef0
0Kelvin <554961401@qq.com>
2021-05-07 16:39:14 +0800

    224. Basic Calculator

--- 224.BasicCalculator/Solution.java
+++ 224.BasicCalculator/Solution.java
@@ -15,7 +15,7 @@ public class Solution {
     public int calculate(String s) {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            if(s.charAt(i)!= ' '){
+            if (s.charAt(i) != ' ') {
                 builder.append(s.charAt(i));
             }
         }
